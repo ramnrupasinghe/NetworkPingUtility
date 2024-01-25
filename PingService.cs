624f498fb42dd993526a46eb98c0512a271df9ae
ramnrupasinghe <ramnrupasinghe@students.nsbm.ac.lk>
2024-01-25 19:22:13 +0530

    NetworkUtility

---
+++ PingService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace pinger_
+{
+    public class PingService
+    {
+        //properties
+
+        public int Data { get; set; }
+
+        public byte[] Buffer { get; set; }
+
+        public int Timeout { get; set; }
+
+        public string Address { get; set; }
+
+        public Ping pingSender { get; set; }
+
+        public PingOptions pingOptions { get; set; }  
+        
+        public PingService() 
+        {
+            Timeout = 120;
+            Address = "4.2.2.2";
+            Buffer = Encoding.ASCII.GetBytes("Network Utility");
+            pingSender = new Ping();
+            pingOptions = new PingOptions();
+            pingOptions.DontFragment = true;    
+        }
+
+
+        public bool SendPing()
+        {
+            PingReply reply = pingSender.Send(Address, Timeout, Buffer, pingOptions);
+
+            if (reply.Status == IPStatus.Success)
+            {
+                Console.WriteLine("Address : {0}", reply.Address.ToString());
+
+                Console.WriteLine("RoundTrip : {0}", reply.RoundtripTime);
+
+                Console.WriteLine("Live Time : {0}", reply.Options.Ttl);
+
+                Console.WriteLine("Don't Fragment : {0}", reply.Options.DontFragment);
+
+                Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pinger_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PingService pingService = new PingService();
+            var response = pingService.SendPing();
+            Console.WriteLine(response);
+
+            Console.ReadLine();
+
+        }
+    }
+}
